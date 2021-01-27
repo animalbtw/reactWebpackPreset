f3d53484e301e591131998255ade589e8a571a8c
animalbtw <animalbtw@gmail.com>
2021-01-27 20:22:23 +0300

    rebuild

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import reactDOM from 'react-dom';
+import './styles/style.scss'
+
+const App = () => {
+  return (
+    <h1>hello asdsadsadasd </h1>
+  )
+}
+
+reactDOM.render(
+  <App />,
+  document.getElementById('root'),
+)
